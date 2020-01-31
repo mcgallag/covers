320e1707fb602f54edc0298ba14222993a8447fe
Mike <mcgallag@gmail.com>
2020-01-31 01:15:20 -0500

    second commit

--- Game.js
+++ Game.js
@@ -17,7 +17,8 @@ define(["require", "exports", "pixijs"], function (require, exports, PIXI) {
             //Add the canvas that Pixi automatically created for you to the HTML document
             document.body.appendChild(this._app.view);
             //load an image and run the `setup` function when it's done
-            PIXI.Loader.shared.load()
+            PIXI.Loader.shared
+                .load()
                 .add(this._imagePath)
                 .load(function () { return _this.Setup(); });
         }
@@ -25,6 +26,10 @@ define(["require", "exports", "pixijs"], function (require, exports, PIXI) {
         Game.prototype.Setup = function () {
             //Create the cat sprite
             var cat = new PIXI.Sprite(PIXI.Loader.shared.resources[this._imagePath].texture);
+            cat.transform.position.x = 100;
+            cat.transform.position.y = 100;
+            cat.transform.pivot.x = cat.width / 2;
+            cat.transform.pivot.y = cat.height / 2;
             //You can also create the `cat` sprite from the texture, like this:
             // let cat = new PIXI.Sprite(PIXI.utils.TextureCache["images/cat.png"]);
             //Add the cat to the stage
--- Game.ts
+++ Game.ts
@@ -1,50 +1,51 @@
-
 import * as PIXI from "pixijs";
 
-export default class Game
-{
-    private _app: PIXI.Application;
-    //private _imagePath = "https://dl.dropboxusercontent.com/s/o962271u2hnx70k/cat.png";
-    private _imagePath = "https://raw.githubusercontent.com/8Observer8/8observer8.github.io/master/Assets/LearnPixiJS/Images/cat.png";
-    
-    public constructor()
-    {
-        //Create a Pixi Application
-        this._app = new PIXI.Application({
-            width: window.innerWidth,
-            height: window.innerHeight,
-            antialias: true,
-            transparent: true,
-            resolution: 1
-        });
+export default class Game {
+  private _app: PIXI.Application;
+  //private _imagePath = "https://dl.dropboxusercontent.com/s/o962271u2hnx70k/cat.png";
+  private _imagePath = "https://raw.githubusercontent.com/8Observer8/8observer8.github.io/master/Assets/LearnPixiJS/Images/cat.png";
 
-        //Add the canvas that Pixi automatically created for you to the HTML document
-        document.body.appendChild(this._app.view);
+  public constructor() {
+    //Create a Pixi Application
+    this._app = new PIXI.Application({
+      width: window.innerWidth,
+      height: window.innerHeight,
+      antialias: true,
+      transparent: true,
+      resolution: 1
+    });
 
-        //load an image and run the `setup` function when it's done
-        PIXI.Loader.shared.load()
-            .add(this._imagePath)
-            .load(() => this.Setup());
-    }
+    //Add the canvas that Pixi automatically created for you to the HTML document
+    document.body.appendChild(this._app.view);
 
-    //This `setup` function will run when the image has loaded
-    private Setup(): void
-    {
-        //Create the cat sprite
-        let cat = new PIXI.Sprite(PIXI.Loader.shared.resources[this._imagePath].texture);
+    //load an image and run the `setup` function when it's done
+    PIXI.Loader.shared
+      .load()
+      .add(this._imagePath)
+      .load(() => this.Setup());
+  }
 
-        //You can also create the `cat` sprite from the texture, like this:
-        // let cat = new PIXI.Sprite(PIXI.utils.TextureCache["images/cat.png"]);
+  //This `setup` function will run when the image has loaded
+  private Setup(): void {
+    //Create the cat sprite
+    let cat = new PIXI.Sprite(PIXI.Loader.shared.resources[this._imagePath].texture);
+    cat.transform.position.x = 100;
+    cat.transform.position.y = 100;
+    cat.transform.pivot.x = cat.width / 2;
+    cat.transform.pivot.y = cat.height / 2;
 
-        //Add the cat to the stage
-        this._app.stage.addChild(cat);
+    //You can also create the `cat` sprite from the texture, like this:
+    // let cat = new PIXI.Sprite(PIXI.utils.TextureCache["images/cat.png"]);
 
-        // This code does not work
-        //If you ever need to, here's how you can clean out WebGL's GPU
-        //memory manually
-        // Object.keys(PIXI.utils.TextureCache).forEach((texture) =>
-        // {
-        //     PIXI.utils.TextureCache[texture].destroy(true);
-        // });
-    }
+    //Add the cat to the stage
+    this._app.stage.addChild(cat);
+
+    // This code does not work
+    //If you ever need to, here's how you can clean out WebGL's GPU
+    //memory manually
+    // Object.keys(PIXI.utils.TextureCache).forEach((texture) =>
+    // {
+    //     PIXI.utils.TextureCache[texture].destroy(true);
+    // });
+  }
 }
